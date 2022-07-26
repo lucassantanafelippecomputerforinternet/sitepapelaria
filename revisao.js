d2c9e5be3abc1eaf74e20bd6194ac35340815942
Lucas Santana Felippe <106406947+lucassantanafelippecomputerforinternet@users.noreply.github.com>
2022-07-26 09:26:03 -0300

    Add files via upload
    commit2

---
+++ revisao.js
@@ -0,0 +1,10 @@
+function soma(num1, num2){
+    let result = num1 + num2;
+    return(result);
+}
+
+function subtracao(num1, num2){
+    //A variável result é de escopo de função!
+    let result = num1 - num2;
+    return(result);
+}
